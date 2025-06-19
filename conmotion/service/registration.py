0d210b04a3708c5dd8a31283d549a0bd44b64ede
A.J. Stein <iam@ajstein.me>
2025-06-19 00:05:23 -0400

    ruff linted TS registration class

--- conmotion/service/registration.py
+++ conmotion/service/registration.py
@@ -15,16 +15,16 @@ class RegistrationPolicyService:
         trust_anchors: Optional[List] = [],
         issuer_ids: Optional[List] = [],
         statement_subjects: Optional[List] = [],
-        private_key_path: Union[str|PathLike]=config.get("service_key_path"),
-        public_cert_path: Union[str|PathLike]=config.get("service_cert_path")
+        private_key_path: Union[str | PathLike] = config.get("service_key_path"),
+        public_cert_path: Union[str | PathLike] = config.get("service_cert_path"),
     ):
         self.is_ready = False
         with (
-           open(private_key_path, "rb") as key_fd,
-           open(public_cert_path, "rb") as cert_fd
+            open(private_key_path, "rb") as key_fd,
+            open(public_cert_path, "rb") as cert_fd,
         ):
-           self.private_key = PrivateKey.from_pem(key_fd.read())
-           self.public_cert = PublicCertificate.from_pem(cert_fd.read())
+            self.private_key = PrivateKey.from_pem(key_fd.read())
+            self.public_cert = PublicCertificate.from_pem(cert_fd.read())
         self.trust_anchors = trust_anchors
         self.issuer_ids = issuer_ids
         self.statement_subjects = statement_subjects
@@ -38,11 +38,11 @@ class RegistrationPolicyService:
     @is_ready.setter
     def is_ready(self, value: bool) -> None:
         if value:
-          logger.debug("registration service is ready now")
+            logger.debug("registration service is ready now")
         else:
-          logger.debug("registration service is not ready now")
+            logger.debug("registration service is not ready now")
         self._is_ready = value
-  
+
     def as_cbor(self) -> IO[bytes]:
         """
         Serialize the current Registration Policy maintained by the service as a
